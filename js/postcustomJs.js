3155a6f9c0869078bf96da650d55aed0a0c8bea0
= <sainyajay@gmail.com>
2016-06-15 11:10:32 +0530

    sd

---
+++ js/postcustomJs.js
@@ -0,0 +1,9 @@
+(function() {
+  var app = angular.module('blogpostApp', ['textAngular']);
+
+  app.controller('PostController', ['$scope', '$http', '$log', function($scope, $http, $log) {
+  
+  }
+  ]);
+
+})();
