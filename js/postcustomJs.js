3a20c4d747001b1f641061e6a29e53bfb6171ef0
Ajay Sainy <sainyajay@gmail.com>
2016-06-15 21:48:00 +0530

    Done

--- js/postcustomJs.js
+++ js/postcustomJs.js
@@ -2,7 +2,9 @@
   var app = angular.module('blogpostApp', ['textAngular']);
 
   app.controller('PostController', ['$scope', '$http', '$log', function($scope, $http, $log) {
-  
+  	var post = this;
+  	post.content = "";
+  	console.log(post.content);
   }
   ]);
 
